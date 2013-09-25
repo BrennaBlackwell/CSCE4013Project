178301ad0b918a5120a167dbf7b9b4f0eb22d6d1
Chandramohan Sol <CXSol91@Gmail.com>
2013-09-25 11:44:46 -0500

    Figured out adding buttons to ActionBar (Look at menu->nav_drawer_test.xml, and notice the onCreateOptionsMenu method in NavDrawerTestActivity.java)

--- spARK/src/edu/uark/csce/groupproject/spark/NavDrawerTestActivity.java
+++ spARK/src/edu/uark/csce/groupproject/spark/NavDrawerTestActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -26,7 +27,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 @SuppressLint("NewApi")
-public class NavDrawerTestActivity extends Activity {
+public class NavDrawerTestActivity extends Activity{
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
