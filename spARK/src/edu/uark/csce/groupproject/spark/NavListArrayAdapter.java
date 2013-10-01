5e753f53689793b5e3ad3434197c2a19ef2ff287
JDPack <jdizzleman18@gmail.com>
2013-10-01 11:45:08 -0500

    Fixed navigation drawer not inflating properly

--- spARK/src/edu/uark/csce/groupproject/spark/NavListArrayAdapter.java
+++ spARK/src/edu/uark/csce/groupproject/spark/NavListArrayAdapter.java
@@ -25,7 +25,6 @@ public class NavListArrayAdapter extends ArrayAdapter<String>{
 		this.context = context;
 		this.resourceID = layoutid;
 		this.options = mListTitles;
-		
 	}
 	
 	@Override
@@ -33,57 +32,51 @@ public class NavListArrayAdapter extends ArrayAdapter<String>{
 		System.out.println("getView " + position + " " + convertView);
 		Drawable icon;
 		//if statement to determine if the position is the account, which requires different layout
-		if (convertView == null) {
-			if (position == 0) {
-				convertView = mInflater.inflate(R.layout.drawer_account, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_menu_profile);
-			}
-			else if (position == 1) {
-				convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_menu_group);
-			}
-			else if (position == 2) {
-				convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_menu_recent);
-			}
-			else if (position == 3) {
-				convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_menu_bookmark);
-			}
-			else if (position == 4) {
-				convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_menu_settings);
-			}
-			else if (position == 5) {
-				convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_menu_feedback);
-			}
-			else if (position == 6) {
-				convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_menu_about);
-			}
-			else if (position == 7) {
-				convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_menu_logout);
-			}
-			else {
-				convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-				icon = convertView.getResources().getDrawable(R.drawable.ic_launcher);
+		//if (convertView == null) {
+			switch (position) {
+				case 0:
+					convertView = mInflater.inflate(R.layout.drawer_account, null);
+					icon = convertView.getResources().getDrawable(R.drawable.ic_menu_profile);
+					break;
+				case 1:
+					convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+					icon = convertView.getResources().getDrawable(R.drawable.ic_menu_group);
+					break;
+				case 2:
+					convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+					icon = convertView.getResources().getDrawable(R.drawable.ic_menu_recent);
+					break;
+				case 3:
+					convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+					icon = convertView.getResources().getDrawable(R.drawable.ic_menu_bookmark);
+					break;
+				case 4:
+					convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+					icon = convertView.getResources().getDrawable(R.drawable.ic_menu_settings);
+					break;
+				case 5:
+					convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+					icon = convertView.getResources().getDrawable(R.drawable.ic_menu_feedback);
+					break;
+				case 6:
+					convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+					icon = convertView.getResources().getDrawable(R.drawable.ic_menu_about);
+					break;
+				default:
+					convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+					icon = convertView.getResources().getDrawable(R.drawable.ic_menu_logout);
+					break;
 			}
 			ImageView imageview = (ImageView) convertView.findViewById(R.id.navListOptionIcon);
 			imageview.setImageDrawable (icon);
-		}
-		else {
+		//}
+		//else {
 			
-		}	
+		//}	
 		TextView textview = (TextView) convertView.findViewById(R.id.navListOptionTextView);
-		
-
 		String s = options[position];
 		textview.setText(s);
 		Log.d(tag, "XML Inflated!");	
-		//just a test for preferences icon
-
 		return convertView;
 	}
 	
