c3df7e28627eb4d46ec6b47049586866c732c695
Chandramohan Sol <CXSol91@Gmail.com>
2013-10-16 11:39:07 -0500

    Removed unnecessary reference to R.java

--- spARK/src/edu/uark/spARK/NavListArrayAdapter.java
+++ spARK/src/edu/uark/spARK/NavListArrayAdapter.java
@@ -9,7 +9,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import edu.uark.spARK.R;
 
 public class NavListArrayAdapter extends ArrayAdapter<String>{
 	private static final String tag = "NavListArrayAdapter";
