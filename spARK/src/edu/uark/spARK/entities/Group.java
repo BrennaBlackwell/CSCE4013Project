b58d15a67ec481e2a8ee5641c62d31e7d09a3096
aadamec <aadamec@uark.edu>
2013-11-21 18:13:52 -0800

    Added open/closed and visible/hidden booleans to Group bean.

--- spARK/src/edu/uark/spARK/entities/Group.java
+++ spARK/src/edu/uark/spARK/entities/Group.java
@@ -8,49 +8,127 @@ import android.location.Location;
 public class Group extends Content {
 	
 	private List<User> users;
+	private final boolean open;
+	private final boolean visible;
 	
 	public Group(int id, String title, String description) {
 		super(id, title, description);
 		this.users = new ArrayList<User>();
+		this.open = true;
+		this.visible = true;
+	}
+	
+	public Group(int id, String title, String description, boolean open, boolean visible) {
+		super(id, title, description);
+		this.users = new ArrayList<User>();
+		this.open = open;
+		this.visible = visible;
 	}
 	
 	public Group(int id, String title, String description, User creator) {
 		super(id, title, description, creator);
 		this.users = new ArrayList<User>();
 		users.add(creator);
+		this.open = true;
+		this.visible = true;
+	}
+	
+	public Group(int id, String title, String description, User creator, boolean open, boolean visible) {
+		super(id, title, description, creator);
+		this.users = new ArrayList<User>();
+		users.add(creator);
+		this.open = open;
+		this.visible = visible;
 	}
 	
 	public Group(int id, String title, String description, User creator, Location location) {
 		super(id, title, description, creator, location);
 		this.users = new ArrayList<User>();
 		users.add(creator);
+		this.open = true;
+		this.visible = true;
+	}
+	
+	public Group(int id, String title, String description, User creator, Location location, boolean open, boolean visible) {
+		super(id, title, description, creator, location);
+		this.users = new ArrayList<User>();
+		users.add(creator);
+		this.open = open;
+		this.visible = visible;
 	}
 	
 	public Group(int id, String title, String description, User creator, Location location, List<User> users) {
 		super(id, title, description, creator, location);
 		this.users = users;
+		this.open = true;
+		this.visible = true;
+	}
+	
+	public Group(int id, String title, String description, User creator, Location location, List<User> users, boolean open, boolean visible) {
+		super(id, title, description, creator, location);
+		this.users = users;
+		this.open = open;
+		this.visible = visible;
 	}
 	
 	public Group(int id, String title, String description, User creator, Date creationDate) {
 		super(id, title, description, creator, creationDate);
 		this.users = new ArrayList<User>();
 		users.add(creator);
+		this.open = true;
+		this.visible = true;
+	}
+	
+	public Group(int id, String title, String description, User creator, Date creationDate, boolean open, boolean visible) {
+		super(id, title, description, creator, creationDate);
+		this.users = new ArrayList<User>();
+		users.add(creator);
+		this.open = open;
+		this.visible = visible;
 	}
 	
 	public Group(int id, String title, String description, User creator, Date creationDate, List<User> users) {
 		super(id, title, description, creator, creationDate);
 		this.users = users;
+		this.open = true;
+		this.visible = true;
+	}
+	
+	public Group(int id, String title, String description, User creator, Date creationDate, List<User> users, boolean open, boolean visible) {
+		super(id, title, description, creator, creationDate);
+		this.users = users;
+		this.open = open;
+		this.visible = visible;
 	}
 	
 	public Group(int id, String title, String description, User creator, Date creationDate, Location location) {
 		super(id, title, description, creator, creationDate, location);
 		this.users = new ArrayList<User>();
 		users.add(creator);
+		this.open = true;
+		this.visible = true;
+	}
+	
+	public Group(int id, String title, String description, User creator, Date creationDate, Location location, boolean open, boolean visible) {
+		super(id, title, description, creator, creationDate, location);
+		this.users = new ArrayList<User>();
+		users.add(creator);
+		this.open = open;
+		this.visible = visible;
 	}
 	
 	public Group(int id, String title, String description, User creator, Date creationDate, Location location, List<User> users) {
 		super(id, title, description, creator, creationDate, location);
 		this.users = users;
+		this.open = true;
+		this.visible = true;
+	}
+	
+	public Group(int id, String title, String description, User creator, Date creationDate, Location location, List<User> users, boolean open, boolean visible) {
+		super(id, title, description, creator, creationDate, location);
+		this.users = users;
+		this.open = open;
+		this.visible = visible;
 	}
 	
 	public String getDescription() {
@@ -59,6 +137,14 @@ public class Group extends Content {
 	
 	public List<User> getUsers() {
 		return users;
+	}
+	
+	public boolean isOpen() {
+		return open;
+	}
+	
+	public boolean isVisible() {
+		return visible;
 	}
 	
 	public void setDescription(String description) {
