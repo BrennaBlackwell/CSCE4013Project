b7730728716c9eaafc777ae6d265b5f0244a1703
JDPack <jdizzleman18@gmail.com>
2013-11-26 12:18:20 -0600

    Updated MapFragment location services

--- spARK/src/edu/uark/spARK/MapView_Fragment.java
+++ spARK/src/edu/uark/spARK/MapView_Fragment.java
@@ -1,32 +1,42 @@
 package edu.uark.spARK;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.*;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
 
-/*We should probably extend MapFragment or SupportMapFragment instead of Fragment so that the class
-itself will take care of alot of the map problems we've been having
-*/
 
-@SuppressLint("NewApi")
-public class MapView_Fragment extends MapFragment {
+public class MapView_Fragment extends MapFragment implements 
+GooglePlayServicesClient.ConnectionCallbacks, 
+GooglePlayServicesClient.OnConnectionFailedListener {
 
 	
 	// Google Map
     private GoogleMap map;	//this is the map which is instantiated in the initializeMap();
 //    private View v;
+    private LocationClient mLocationClient;
+
     
     public MapView_Fragment() {
 
@@ -35,6 +45,7 @@ public class MapView_Fragment extends MapFragment {
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(this.getActivity(), this, this);
     }
     
     @Override
@@ -52,6 +63,9 @@ public class MapView_Fragment extends MapFragment {
         } catch (Exception e) {
             e.printStackTrace();
         }
+//	    final float scale = this.getResources().getDisplayMetrics().density;
+//	    int pixels = (int) (100 * scale + 0.5f);
+//	    v.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, pixels));
     	return v;
     }       	
 
@@ -62,18 +76,24 @@ public class MapView_Fragment extends MapFragment {
     		map = getMap();
             map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             map.setMyLocationEnabled(true);
-            //this causes an error message in Logcat
-            //map.setMyLocationEnabled(true);
-            CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(36.065853, -94.173818)).zoom(18).build();
-            map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            
-            //MAP IS ALREADY CREATED SO WE DONT REALLY NEED THIS (DO WE?)
-//            // check if map is created successfully or not
-//            if (map == null) {
-//                Toast.makeText(getActivity().getApplicationContext(),
-//                        "Map could not be created", Toast.LENGTH_SHORT)
-//                        .show();
-//            }
+
+    }
+    
+    @Override
+	public void onStart() {
+        super.onStart();
+        // Connect the client.
+        if(isGooglePlayServicesAvailable()){
+            mLocationClient.connect();
+        }
+
+    }
+    
+    @Override
+	public void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
     }
  
     @Override
@@ -88,14 +108,90 @@ public class MapView_Fragment extends MapFragment {
     }
         
     @Override
-    public void onDestroyView() {
-    	
+    public void onDestroyView() { 	
         super.onDestroyView();
-//        MapFragment m = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-//
-//        if (m != null)
-//        	getFragmentManager().beginTransaction().remove(m).commit();
-//
     }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this.getActivity(),
+                        9000);
+                /*
+                * Thrown if Google Play services canceled the original
+                * PendingIntent
+                */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+           Toast.makeText(getActivity().getApplicationContext(), "Sorry. Location services not available to you", Toast.LENGTH_LONG).show();
+        }
+    }
+
+	@Override
+	public void onConnected(Bundle dataBundle) {
+	    // Display the connection status
+	    Toast.makeText(this.getActivity(), "Connected to gps services", Toast.LENGTH_SHORT).show();
+	    Location location = mLocationClient.getLastLocation();
+	    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+	    CameraPosition cameraPosition = new CameraPosition.Builder()
+		.target(latLng)
+		.zoom(18)
+		.build();
+	    map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+	    //CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+	    //map.animateCamera(cameraUpdate);
+	}
+
+	@Override
+	public void onDisconnected() {
+	    // Display the connection status
+	    Toast.makeText(this.getActivity(), "Disconnected from gps services. Please re-connect.",
+	            Toast.LENGTH_SHORT).show();
+	}
+	
+	private boolean isGooglePlayServicesAvailable() {
+	    // Check that Google Play services is available
+	    int resultCode =  GooglePlayServicesUtil.isGooglePlayServicesAvailable(this.getActivity());
+	    // If Google Play services is available
+	    if (ConnectionResult.SUCCESS == resultCode) {
+	        // In debug mode, log the status
+	        Log.d("Location Updates", "Google Play services is available.");
+	        return true;
+	    } else {
+
+	        return false;
+	    }
+	}
     
+	@Override
+	public void onActivityResult(
+	                int requestCode, int resultCode, Intent data) {
+	    // Decide what to do based on the original request code
+	    switch (requestCode) {
+
+	        case 9000:
+	            /*
+	             * If the result code is Activity.RESULT_OK, try
+	             * to connect again
+	             */
+	            switch (resultCode) {
+	                case Activity.RESULT_OK:
+	                    mLocationClient.connect();
+	                    break;
+	            }
+
+	    }
+	}
 }
